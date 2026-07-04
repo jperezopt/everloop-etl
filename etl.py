ecd47a123b7450be1f89414e67e985a04817de98
J Perez <jperezopt@gmail.com>
2026-07-04 17:39:39 -0500

    add pipeline scope docstring

---
+++ etl.py
@@ -0,0 +1,7 @@
+'''
+extract-transform-load script for everloop's csv databases -> supabase
+
+Needs to take multiple csv files as input (preferably via a tolerable GUI),
+validate the csv files, transform them with lowercase/property priority/keys,
+and finally load them into supabase.
+'''
