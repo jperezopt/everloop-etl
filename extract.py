25b0cd7a358c242d03203221d8297c0aaa78b6fa
J Perez <jperezopt@gmail.com>
2026-07-07 14:59:22 -0500

    add extraction logic and wrapper

--- extract.py
+++ extract.py
@@ -1,28 +1,46 @@
 from pathlib import Path
+import pandas as pd
 
-EXPECTED_DIRECTORY = "adalo_csv_database_collections"
-
-EXPECTED_FILENAMES = {
-    "Containers Out.csv",
-    "Cup Stock Updates.csv",
-    "Lost and Broken Cups.csv",
+EXPECTED_DIR = "adalo_csv_database_collections"
+EXPECTED_FILES = {
+    "Containers_Out.csv",
+    "Cup_Stock_Updates.csv",
+    "Lost_and_Broken_Cups.csv",
     "Region.csv",
     "Reports.csv",
-    "Site Inventory Tracking.csv",
+    "Site_Inventory_Tracking.csv",
     "Sites.csv",
-    "User History.csv",
-    "User Payment Flow.csv"
-    "User Payment History.csv",
-    "Users.csv"}
+    "User_History.csv",
+    "User_Payment_Flow.csv",
+    "User_Payment_History.csv",
+    "Users.csv",
+}
 
-def verify_csv_files(EXPECTED_DIRECTORY, EXPECTED_FILENAMES):
-    cwd = Path(__file__).parent
-    actual_directory = cwd / EXPECTED_DIRECTORY
-    if not actual_directory.is_dir():
-        raise FileNotFoundError(
-            f"could not find '{EXPECTED_DIRECTORY}' in '{cwd}'")
-    actual_filenames = {item.name for item in actual_directory.iterdir()}
-    missing = EXPECTED_FILENAMES - actual_filenames
-    extra = actual_filenames - EXPECTED_FILENAMES
+
+def extract(
+    expected_dir: str = EXPECTED_DIR, expected_files: set[str] = EXPECTED_FILES
+) -> dict[str, pd.DataFrame]:
+    actual_dir = Path(__file__).parent / expected_dir
+    verify_csv_files(actual_dir, expected_files)
+    return extract_csv_files(actual_dir, expected_files)
+
+
+def verify_csv_files(actual_dir: Path, expected_files: set[str]) -> None:
+    if not actual_dir.is_dir():
+        raise FileNotFoundError(f"could not find '{actual_dir}'")
+
+    actual_files = {item.name for item in actual_dir.iterdir()}
+    missing = expected_files - actual_files
+    extra = actual_files - expected_files
+
     if missing or extra:
         raise ValueError(f"missing {sorted(missing)}, extra: {sorted(extra)}")
+
+
+def extract_csv_files(
+    actual_dir: Path, expected_files: set[str]
+) -> dict[str, pd.DataFrame]:
+    databases = {}
+    for file in expected_files:
+        databases[Path(file).stem] = pd.read_csv(actual_dir / file)
+    return databases
--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-'''
-extract-transform-load script for everloop's csv databases -> supabase
-
-Needs to take multiple csv files as input (preferably via a tolerable GUI),
-validate the csv files, transform them with lowercase/property priority/keys,
-and finally load them into supabase.
-'''
 import extract as ex
 
+
 def main():
-    ex.verify_csv_files(ex.EXPECTED_DIRECTORY, ex.EXPECTED_FILENAMES)
+    databases = ex.extract()
+    print(databases["Users"])
+
 
 if __name__ == "__main__":
     main()
