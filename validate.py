ce4d85b928c5e9493f02fc2792e27a4828a76d5d
J Perez <jperezopt@gmail.com>
2026-07-10 11:50:32 -0500

    rewrite validate to schema whitelist design

--- validate.py
+++ validate.py
@@ -1,16 +1,15 @@
 import pandas as pd
 
-from schema_config import EXPECTED_PROPERTIES
+from schema_config import SCHEMA_WHITELIST
 
 
-def validate_schemas(databases: dict[str, pd.DataFrame]) -> None:
+# Ensure the whitelisted columns/properties are present in the databases
+def validate(databases: dict[str, pd.DataFrame]) -> None:
     for db, df in databases.items():
-        expected_props = EXPECTED_PROPERTIES[db]
         actual_props = {
-            prop.lower().replace("- ", "").replace(" ", "_")
-            for prop in set(df.columns)
+            prop.lower().replace(" ", "_") for prop in set(df.columns)
         }
-        missing = expected_props - actual_props
-        extra = actual_props - expected_props
-        if missing or extra:
-            raise ValueError(f"missing: {missing}, extra: {extra}")
+        needed_props = SCHEMA_WHITELIST[db]
+        missing = needed_props - actual_props
+        if missing:
+            raise ValueError(f"missing properties: {db, missing}")
