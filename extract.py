942da1becd709471be4ca6a13ae0f5f140d0e3d2
J Perez <jperezopt@gmail.com>
2026-07-09 20:53:04 -0500

    rewrite extract to schema whitelist design

--- extract.py
+++ extract.py
@@ -2,33 +2,48 @@ import pandas as pd
 from pathlib import Path
 
 from schema_config import EXPECTED_DIR
-from schema_config import EXPECTED_FILES
+from schema_config import SCHEMA_WHITELIST
 
 
 def extract(
-    expected_dir: str = EXPECTED_DIR, expected_files: set[str] = EXPECTED_FILES
+    expected_dir: str = EXPECTED_DIR,
+    schema_whitelist: dict[str, set[str]] = SCHEMA_WHITELIST,
 ) -> dict[str, pd.DataFrame]:
     actual_dir = Path(__file__).parent / expected_dir
-    verify_csv_files(actual_dir, expected_files)
-    return extract_csv_files(actual_dir, expected_files)
+    actual_files = verify_csv_files(actual_dir, schema_whitelist)
+    return extract_csv_files(actual_files)
 
 
-def verify_csv_files(actual_dir: Path, expected_files: set[str]) -> None:
+def verify_csv_files(
+    actual_dir: Path, schema_whitelist: dict[str, set[str]]
+) -> dict[str, Path]:
     if not actual_dir.is_dir():
         raise FileNotFoundError(f"could not find '{actual_dir}'")
 
-    actual_files = {item.name for item in actual_dir.iterdir()}
-    missing = expected_files - actual_files
-    extra = actual_files - expected_files
+    actual_files = {}
+    invalid_items = []
+    for item in actual_dir.iterdir():
+        if item.is_file() and item.suffix.lower() == ".csv":
+            actual_files[item.stem.lower()] = item
+        else:
+            invalid_items.append(item.name)
 
-    if missing or extra:
-        raise ValueError(f"missing {sorted(missing)}, extra: {sorted(extra)}")
+    expected_filenames = set(schema_whitelist.keys())
+    actual_filenames = set(actual_files.keys())
+    missing = expected_filenames - actual_filenames
+    extra = actual_filenames - expected_filenames
+
+    if missing or extra or invalid_items:
+        raise ValueError(
+            f"missing files: {sorted(missing)}, "
+            f"extra files: {sorted(extra)}, "
+            f"invalid items: {sorted(invalid_items)}"
+        )
+
+    return actual_files
 
 
 def extract_csv_files(
-    actual_dir: Path, expected_files: set[str]
+    actual_files: dict[str, Path],
 ) -> dict[str, pd.DataFrame]:
-    databases = {}
-    for file in expected_files:
-        databases[Path(file).stem] = pd.read_csv(actual_dir / file)
-    return databases
+    return {stem: pd.read_csv(path) for stem, path in actual_files.items()}
