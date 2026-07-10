63d60769d557c67a8bb378430a0e749ac59a6807
J Perez <jperezopt@gmail.com>
2026-07-10 12:56:00 -0500

    rewrite transform to schema whitelist design

--- transform.py
+++ transform.py
@@ -1,20 +1,22 @@
 import pandas as pd
 
+from schema_config import SCHEMA_WHITELIST
+
 
 def transform(databases: dict[str, pd.DataFrame]) -> dict[str, pd.DataFrame]:
+    normalized_databases = normalize_df(databases)
+    filter_whitelist_cols(normalized_databases)
+    return normalized_databases
+
+
+def normalize_df(
+    databases: dict[str, pd.DataFrame],
+) -> dict[str, pd.DataFrame]:
     for df in databases.values():
-        normalize_df(df)
-        delete_df_cols(df)
-    return databases
-
-
-def normalize_df(df: pd.DataFrame) -> None:
-    df.columns = (
-        df.columns.str.replace("- ", "").str.replace(" ", "_").str.lower()
-    )
-    string_df = df.select_dtypes(include=str)
-    for col in string_df.columns:
-        df[col] = string_df[col].str.lower().str.replace(" ", "_")
+        df.columns = df.columns.str.lower().str.replace(" ", "_")
+        string_df = df.select_dtypes(include=str)
+        for col in string_df.columns:
+            df[col] = string_df[col].str.lower().str.replace(" ", "_")
 
     # Fun row-wise version
     # num_rows = df.index.stop
@@ -24,7 +26,13 @@ def normalize_df(df: pd.DataFrame) -> None:
     #         lambda x: x.lower().replace(" ", "_") if isinstance(x, str) else x
     #     )
     #     print(df.iloc[i])
+    return databases
 
 
-def delete_df_cols(df: pd.DataFrame) -> None:
-    return None
+# These columns are not relevant to analysis. Must remove them to fit the
+# schemas defined on supabase.
+def filter_whitelist_cols(
+    normalized_databases: dict[str, pd.DataFrame],
+) -> None:
+    for db, df in normalized_databases.items():
+        normalized_databases[db] = df[list(SCHEMA_WHITELIST[db])]
