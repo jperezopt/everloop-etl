31c0fb9339c1dfdd99023cb35cbfa6f8bea15ff4
J Perez <jperezopt@gmail.com>
2026-07-10 15:10:27 -0500

    refactor: delete non-packaged etl modules

--- load.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-from supabase import create_client
-
-load_dotenv()
-url = os.environ["SUPABASE_URL"]
-key = os.environ["SUPABASE_KEY"]
-
-client = create_client(url, key)
